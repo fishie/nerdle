70eb96526fd9759ff14d03e5a110cd867c91851b
Rishie <rishie@rishie.se>
2023-06-08 21:24:57 +0200

    Refactor

--- nerdle.py
+++ nerdle.py
@@ -15,12 +15,8 @@ def generate_valid_expressions(previous_symbols, desired_length, expressions=set
             pass
         return
     possible_symbols = available_symbols
-    if len(previous_symbols) == desired_length - 2:
-        try:
-            if int(previous_symbols):
-                possible_symbols = '+-*/'
-        except ValueError:
-            pass
+    if len(previous_symbols) == desired_length - 2 and previous_symbols.isnumeric():
+        possible_symbols = '+-*/'
     if previous_symbols == '' or previous_symbols[-1] in '+-':
         possible_symbols = '123456789'
     elif previous_symbols[-1] in '*/':
